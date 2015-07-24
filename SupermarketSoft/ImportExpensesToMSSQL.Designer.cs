b11adaf5f7e89b46a0655f3ec7e8bccc0de898e3
Todorov96 <todorov.georgi9@abv.bg>
2015-07-24 14:44:40 +0300

    FormUpdate

--- SupermarketSoft/ImportExpensesToMSSQL.Designer.cs
+++ SupermarketSoft/ImportExpensesToMSSQL.Designer.cs
@@ -28,11 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ImportXMLToMSSQL";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // ImportExpensesToMSSQL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 164);
+            this.Controls.Add(this.button1);
+            this.Name = "ImportExpensesToMSSQL";
             this.Text = "ImportExpensesToMSSQL";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
