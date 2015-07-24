91aa57c85a57bf240b37d3fc89b12510c31877c6
Todorov96 <todorov.georgi9@abv.bg>
2015-07-24 14:15:51 +0300

    expense

---
+++ Supermarket.Models/Expense.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int VendorId { get; set; }
+
+        public virtual Vendor Vendor{get;set;}
+
+        public DateTime DateOfExpense { get; set; }
+
+        public decimal Value { get; set; }
+    }
+}
